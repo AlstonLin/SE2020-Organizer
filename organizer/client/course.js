baab6ba64e299da04da80a236702bdddabb69ec1
James Hageman <jamesd@hageman.ca>
2015-09-17 13:38:23 -0400

    Link to course page

---
+++ organizer/client/course.js
@@ -0,0 +1,3 @@
+Template.course.helpers({
+
+})
--- organizer/shared/routes.js
+++ organizer/shared/routes.js
@@ -5,3 +5,13 @@ Router.configure({
 Router.route('/', {
   template: 'home'
 });
+
+Router.route('/course/:code', function () {
+  this.render('course', {
+    data: function () {
+      return {
+        course: Courses.findOne({code: this.params.code})
+      };
+    }
+  })
+});
