2463410827d9139f4f79578ed406d12f06b82b2c
James Hageman <jamesd@hageman.ca>
2015-09-17 13:40:19 -0400

    Fix course code

--- organizer/server/startup.js
+++ organizer/server/startup.js
@@ -5,7 +5,7 @@ Meteor.startup(function () {
     Courses.insert({name: 'Linear Circuits', code: 'ECE 140'});
     Courses.insert({name: 'Physics', code: 'ECE 105'});
     Courses.insert({name: 'Linear Algebra', code: 'MATH 115'});
-    Courses.insert({name: 'Calculus 1', code: 'MATH 115'});
+    Courses.insert({name: 'Calculus 1', code: 'MATH 117'});
     Courses.insert({name: 'Methods of Software Engineering', code: 'SE 101'});
   }
 });
