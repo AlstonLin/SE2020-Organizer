34328a7d7d2a57f97e49eaa7776916b37517765b
James Hageman <jamesd@hageman.ca>
2015-09-20 13:54:15 -0400

    Fix modal enter bug, adjust code formatting

--- organizer/client/controllers/course.js
+++ organizer/client/controllers/course.js
@@ -1,51 +1,66 @@
 Template.course.helpers({
-  assignments: function () {
-    return Assignments.find({course_code: this.course.code});
+  assignments: function() {
+    return Assignments.find({
+      course_code: this.course.code
+    });
   },
 
-  isCompleted: function () {
-  	return this.completed == null ? false : (this.completed.indexOf(Meteor.userId()) !== -1);
+  isCompleted: function() {
+    return this.completed == null ? false : (this.completed.indexOf(Meteor.userId()) !== -1);
   },
 
-  isReading: function(){
+  isReading: function() {
     return this.label == 0;
   },
 
-  isNotGraded: function(){
+  isNotGraded: function() {
     return this.label == 1;
   },
 
-  isGraded: function(){
+  isGraded: function() {
     return this.label == 2;
   },
 
 });
 
-Template.course.onRendered(function () {
+Template.course.onRendered(function() {
   this.$('[data-toggle="tooltip"]').tooltip();
 });
 
 Template.course.events({
-	"click .toggle-completed" : function(event){ //Adds the user to the list of users who have completed
-    if (this.completed == null) { this.completed = [];}
-    if (event.target.checked){ //Checked completed
+  "click .toggle-completed": function(event) { //Adds the user to the list of users who have completed
+    if (this.completed == null) {
+      this.completed = [];
+    }
+    if (event.target.checked) { //Checked completed
       this.completed.push(Meteor.userId());
     } else { //Unchecked
       delete this.completed[this.completed.indexOf(Meteor.userId())];
     }
-    Assignments.update(this._id, {$set: {completed: this.completed}})
-	},
+    Assignments.update(this._id, {
+      $set: {
+        completed: this.completed
+      }
+    })
+  },
 
-  "submit .new-comment" : function(event){ //NOTE: BUG -> PRESSING ENTER CAUSES MODAL TO CLOSE
+  "submit .new-comment": function(event) {
     var text = event.target.text.value;
     var comment = {
       author: Meteor.user().profile.name,
       date: new Date(),
       message: text
     };
-    if (this.comments == null){this.comments = []}
+    if (this.comments == null) {
+      this.comments = []
+    }
     this.comments.push(comment);
-    Assignments.update(this._id, {$set: {comments: this.comments}})
+    Assignments.update(this._id, {
+      $set: {
+        comments: this.comments
+      }
+    })
     event.target.text.value = "";
+    event.preventDefault();
   }
 })
