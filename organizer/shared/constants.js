6f95710dd02455f746b65fa65d177a08dcc9fa4b
Will-Lo <will-i-am_95@hotmail.com>
2015-09-30 21:23:16 -0400

    Fixed Typos
    Now it should work, whoops

--- organizer/shared/constants.js
+++ organizer/shared/constants.js
@@ -2,7 +2,7 @@ Constants = {
 	groupId: "464502600377423",
 };
 
-Calendar_account = (
-	email: "SE2020Organizer@gmail.com"
+Calendar_account = {
+	email: "SE2020Organizer@gmail.com",
 	password: "waterloose2020"
-);
+};
