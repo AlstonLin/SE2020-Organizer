12578fd1a2799a58382ca4fc4bab09e32f8e2a1c
tylerjxzhang <tylerzhang@outlook.com>
2015-10-01 14:56:04 -0400

    Updated UI and created homepage layout

--- organizer/client/controllers/nav.js
+++ organizer/client/controllers/nav.js
@@ -1,11 +1,14 @@
 Template.nav.helpers({
-  courses: function () {
-    return Courses.find();
-  },
-  showBadge: function (code) {
-    return !!Assignments.findOne({course_code: code});
-  },
-  getBadge: function (code) {
-    return Assignments.find({course_code: code}).count();
-  }
+	getAvatarURL: function () {
+		return Methods.getFacebookAvatarURL(Meteor.userId());
+	},
+  	courses: function () {
+    	return Courses.find();
+  	},
+	  showBadge: function (code) {
+	    return !!Assignments.findOne({course_code: code});
+	  },
+	  getBadge: function (code) {
+	    return Assignments.find({course_code: code}).count();
+	  }
 });
